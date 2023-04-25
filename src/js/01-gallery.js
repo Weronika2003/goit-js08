a144eb2f77ecc631ea8f8f12cac36fc3df7bba57
Weronika2003 <creative.name2003@gmail.com>
2023-04-25 21:31:11 +0200

    hm

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -5,3 +5,16 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+const img = document.querySelector('.gallery');
+
+const showImg = galleryItems
+  .map(
+    galleryItems => `<li class="gallery__item"><a class="gallery__link" href="${galleryItems.original}">
+  <img class="gallery__image" src="${galleryItems.preview}" alt="${galleryItems.description}" title="${galleryItems.description}"/>
+</a></li>`
+  )
+  .join('');
+img.innerHTML = showImg;
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionDelay: 250,
+});
